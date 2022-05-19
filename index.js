cba4bf17979e16414d5dc68fbd7e6095591d3986
clarxyg <clarxyg@gmail.com>
2022-05-19 14:08:06 -0300

    change slidesPerView in mobile

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const swiper = new Swiper('.swiper-container', {
   keyboard: true,
   breakpoints: {
     767: {
-      slidesPerView: 2,
+      slidesPerView: 1,
       setWrapperSize: true
     }
   }
